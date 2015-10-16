66bc2adcede6216060d222d01bac9d0350c13be9
Cyrille M <cmeichel@free.fr>
2015-10-16 19:25:13 +0200

    organized doc

--- doc/src/scripts/services/conf-loader.service.js
+++ doc/src/scripts/services/conf-loader.service.js
@@ -1,13 +1,24 @@
 angular.module('Documentation').service('ConfLoader', function ($resource, appConfiguration, $q) {
     return function() {
+        var getMainRoute = function(route) {
+            var matcher = route.match(/^(\/\w*).*/);
+            if (matcher) {
+                return  matcher[1];
+            }
+        };
         return $q(function(resolve, reject) {
             $resource(appConfiguration.url).get().$promise.then(
                 function (data) {
                     var result = {};
                     Object.keys(data.routes).forEach(function(route){
-                        var mainRoute = route.replace(/\/?api/, '');
-                        
-                        result[mainRoute] = data.routes[route];
+                        var cleanRoute = route.replace(/^\/?api/, '');
+                        var mainRoute = getMainRoute(cleanRoute);
+                        if (!result[mainRoute]) {
+                            result[mainRoute] = {
+                                data: {}
+                            };
+                        }
+                        result[mainRoute].data[cleanRoute] = data.routes[route];
                     });
                     resolve(result);
                 }, reject
